7784e1656daffae22047ce0ed49e6a93a9314096
vladimir_tsimbol <yuashick@gmail.com>
2019-06-17 20:28:43 +0500

    рефакторинг

--- venv/Scripts/Activate.ps1
+++ venv/Scripts/Activate.ps1
@@ -27,7 +27,7 @@ function global:deactivate ([switch]$NonDestructive) {
 
 deactivate -nondestructive
 
-$env:VIRTUAL_ENV="D:\Vladimir\Github\rossmann\venv"
+$env:VIRTUAL_ENV="D:\Github\rossmann\venv"
 
 if (! $env:VIRTUAL_ENV_DISABLE_PROMPT) {
     # Set the prompt to include the env name
